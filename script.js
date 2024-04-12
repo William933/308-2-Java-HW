0c64fc0ad92affd6fa9d38bf93a1f3b99e0254f2
William Allen <williamallen933@gmail.com>
2024-04-12 19:23:38 -0400

    ran week 1-3

--- script.js
+++ script.js
@@ -8,7 +8,34 @@ const areaStart = 0.8;
 const Area=(PI* Radius* Radius); 
 console.log({Area})
 
-{
-    "Area": 78.53750000000001
-}
+//"Area": 78.53750000000001
 
+///////////////
+
+/////Percentage  Week 1
+const Week = '40%'
+if (Week >= "80%") {
+	console.log("Pruned")
+} else if (Week <= "79%") {
+	console.log("Monitored")
+} else if (Week <= "50%"){
+	console.log("Planted")
+}
+////Week2
+const Week2 = '70%'
+if (Week2 >= "80%") {
+	console.log("Pruned")
+} else if (Week2 <= "79%") {
+	console.log("Monitored")
+} else if (Week2 <= "50%"){
+	console.log("Planted")
+}
+/////Week3
+const Week3 = '85%'
+if (Week3 >= "80%") {
+	console.log("Pruned")
+} else if (Week3 <= "79%") {
+	console.log("Monitored")
+} else if (Week3 <= "50%"){
+	console.log("Planted")
+}
